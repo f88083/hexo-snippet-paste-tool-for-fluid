90d89ac386d8f5d97d115b08711a8efa4107bf85
f88083 <f88083@gmail.com>
2024-03-14 21:45:32 +0000

    Feat: refactor var. name, added "foldBlockCommand" for Fold Block

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,27 @@
 import * as vscode from 'vscode';
 import { youtubeEmbedPaste } from './util/youtubeEmbed';
+import { foldBlock } from './util/foldBlock';
 
 export function activate(context: vscode.ExtensionContext) {
-	// Register the command
-	let disposable = vscode.commands.registerCommand('hexo-syntax-paste-tool-for-fluid.pasteYoutubeEmbed', () => {
+	// Register multiple commands
+	let youtubeEmbedCommand = vscode.commands.registerCommand('hexo-syntax-paste-tool-for-fluid.pasteYoutubeEmbed', () => {
 		youtubeEmbedPaste();
 	});
 
+	let foldBlockCommand = vscode.commands.registerCommand('hexo-syntax-paste-tool-for-fluid.pasteFoldBlock', () => {
+		foldBlock();
+	});
+	
+
 	// Add the command to the context
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(youtubeEmbedCommand);
+	context.subscriptions.push(foldBlockCommand);
 }
+
+// Show all the paste options
+// async function showOptions() {
+// 	vscode.window.showInputBox({
+// }
 
 
 // This method is called when your extension is deactivated
